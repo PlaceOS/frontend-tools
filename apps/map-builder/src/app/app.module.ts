70c536667aeffebe47af571e442866c0ffed8125
Alex Sorafumo <alex@yuion.net>
2021-03-10 12:29:48 +1100

    chore(package): add angular pwa, material and localize to map-builder

--- apps/map-builder/src/app/app.module.ts
+++ apps/map-builder/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
