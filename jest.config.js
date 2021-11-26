06f6dbc8ddd5d0d4ef6ece250de615bff4275b7d
Alex Sorafumo <alex@yuion.net>
2021-11-26 13:28:27 +1100

    chore(project): update nx project to latest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
-module.exports = {
-    projects: [
-        '<rootDir>/apps/map-builder',
-        '<rootDir>/libs/common',
-        '<rootDir>/libs/components',
-        '<rootDir>/apps/map-regions',
-        '<rootDir>/apps/sensor-map',
-        '<rootDir>/apps/app-settings',
-    ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
