103ae8f6f811578477413a06e9124c7763e3f038
Alex Sorafumo <alex@yuion.net>
2024-02-05 16:13:52 +1100

    chore: tweak iFrame message parsing

--- libs/common/src/lib/backoffice.fn.ts
+++ libs/common/src/lib/backoffice.fn.ts
@@ -1,6 +1,8 @@
+import { randomString } from './general';
 import type { HashMap } from './types';
 
 export interface FrameMessage {
+    id?: string;
     type: 'backoffice';
     action: 'update' | 'metadata' | 'resource';
     name?: string;
@@ -43,21 +45,30 @@ export function retrieveData<T = HashMap>(
     });
 }
 
+interface PromiseMethods {
+    resolve: (value: any) => void;
+    reject: (reason?: any) => void;
+}
+const resolve_map: Record<string, PromiseMethods> = {};
+
+export function onMessage(m: any) {
+    if (typeof m.data !== 'string') return;
+    const parsed: FrameMessage = JSON.parse(m.data);
+    if (!parsed.id && resolve_map[parsed.id]) return;
+    const { resolve, reject } = resolve_map[parsed.id];
+    if (parsed && parsed.type === 'backoffice') {
+        parsed.status === 'success' ? resolve(parsed.content) : reject(parsed);
+        delete resolve_map[parsed.id];
+    }
+}
+window.addEventListener('message', onMessage);
+
 export function sendMessage(msg: FrameMessage) {
     return new Promise<any>((resolve, reject) => {
         if (isChildFrame()) {
+            if (!msg.id) msg.id = randomString(8);
             window.parent.postMessage(JSON.stringify(msg), '*');
-            const onMessage = (m: any) => {
-                if (typeof m.data !== 'string') return;
-                const parsed: FrameMessage = JSON.parse(m.data);
-                if (parsed && parsed.type === 'backoffice') {
-                    parsed.status === 'success'
-                        ? resolve(parsed.content)
-                        : reject(parsed);
-                    window.removeEventListener('message', onMessage);
-                }
-            };
-            window.addEventListener('message', onMessage);
+            resolve_map[msg.id] = { resolve, reject };
         } else {
             reject('Application is not in an iFrame.');
         }
